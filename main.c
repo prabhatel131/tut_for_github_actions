f2a9dde6b12bf7e6b09efd120fc0cd2dbdfb7e3f
prabhatel131 <126231368+prabhatel131@users.noreply.github.com>
2024-01-03 18:03:16 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,21 @@
-User
+
 #include <stdio.h>
 
-int main() {
-    // Define an array with 10 elements
-    int array[10];
+int main() 
+{
+        int array[10];
 
-    // Prompt the user to input values for the array
-    printf("Enter 10 elements for the array:\n");
-
-    // Loop to input values into the array
-    for (int i = 0; i < 10; i++) {
-        printf("Element %d: ", i + 1);
-        scanf("%d", &array[i]);
-    }
-
-    // Calculate the sum of the elements
+        printf("Enter 10 elements for the array:\n");
+        for (int i = 0; i < 10; i++)
+        {
+            printf("Element %d: ", i + 1);
+            scanf("%d", &array[i]);
+        }
     int sum = 0;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) 
+    {
         sum += array[i];
     }
-
-    // Display the sum
     printf("Sum of the elements: %d\n", sum);
 
     return 0;
