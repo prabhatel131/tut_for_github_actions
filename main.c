d14f01ee9ac6fb6236466631489c6612c4e6458c
prabhatel131 <126231368+prabhatel131@users.noreply.github.com>
2024-01-03 18:31:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main()
             scanf("%d", &array[i]);
         }
     int sum = 0;
-    for (int i = 0; i < 10; i++) 
+    for (int i = 0; i < 10; p++) 
     {
         sum += array[i];
     }
